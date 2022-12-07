8f689ad64ac8f223a2de131346a5566e4e6a4aa3
enei6776 <117900418+enei6776@users.noreply.github.com>
2022-12-07 15:55:21 +0100

    created the project
    created the .net project

---
+++ mangonet/mangonet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mangonet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
